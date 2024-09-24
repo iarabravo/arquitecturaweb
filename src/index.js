df2ebd091a44a6923165c8d10c12ef499fd21d54
iarabravo <iarabravo2017@hotmail.com>
2024-09-24 01:42:48 -0300

    Mensaje describiendo los cambios

--- src/index.js
+++ src/index.js
@@ -1,5 +1,15 @@
-const express = requiere ("express");
+const express = require ("express");
+const morgan = require ("morgan");
+
+//Configuration
 const app = express();
 app.set("port", 4000);
+app.use(morgan("dev"));
 app.listen(app.get("port"));
 console.log("Escuchando comunicaciones al puerto "+app.get("port"));
+
+//Middlewares
+app.get("/productos",(req,res)=>
+{
+    res.send("Mensaje recibido")
+})
