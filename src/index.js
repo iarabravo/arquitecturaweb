ca5f804bd509da08fd8b98a4355e1ae1ec3e3412
iarabravo <iarabravo2017@hotmail.com>
2024-09-24 16:01:55 -0300

    Mensaje describiendo los cambios

--- src/index.js
+++ src/index.js
@@ -58,37 +58,46 @@ app.get('/', function(request, response) {
 
 });
 
-/*app.post("/habitaciones", async (req, res) => {
-    console.log("Ruta /habitaciones (POST) llamada");
-
-    let connection;
-    try {
-        connection = await database.getConnection();
-        console.log("Conexión a la base de datos establecida");
-
-        // Obtener los datos del cuerpo de la solicitud
-        const { tipo, precio } = req.body; // Asegúrate de enviar estos datos en el cuerpo de la solicitud
-        console.log("Datos recibidos:", { tipo, precio });
-
-        // Verifica si todos los campos necesarios están presentes
-        if (!tipo || !precio) {
-            return res.status(400).json({ error: "Faltan datos requeridos" });
+app.get("/clientes",async(req,res)=>
+    {
+        console.log("Ruta /clientes llamada"); // Verificar llamada a la ruta
+        const connection = await database.getConnection();
+        console.log("Conexión a la base de datos establecida"); // Verificar conexión
+        const dni = req.query.dni; // Parámetro para dni del cliente
+        const nombre = req.query.nombre; // Parámetro para nombre del cliente
+        const apellido = req.query.apellido; // Parámetro para apellido del cliente
+        console.log("Dni del cliente recibido: ", dni); // Verificar dni recibido
+        console.log("Nombre del cliente recibido: ", nombre); // Verificar nombre recibido
+        console.log("Apellido del cliente recibido: ", apellido); // Verificar apellido recibido
+        let query = "SELECT * FROM CLIENTE";
+        const params = [];
+        // Verificar si se proporciona un ID o dni
+        if (nombre) {
+            query += " WHERE nombre = ?";
+            params.push(id);
+            console.log("Condición de nombre añadida a la consulta"); // Verificar condición
+        } else if (dni) {
+            query += " WHERE dni = ?";
+            params.push(dni);
+            console.log("Condición de dni añadida a la consulta"); // Verificar condición
+        } else if (apellido) {
+            query += " WHERE apellido = ?";
+            params.push(apellido);
+            console.log("Condición de dni añadida a la consulta"); // Verificar condición
         }
-
-        // Construir la consulta SQL
-        const query = "INSERT INTO HABITACION (tipo, precio) VALUES (?, ?)";
-        const params = [tipo, parseFloat(precio)]; // Convertir precio a número
-        console.log("Query: ", query);
-        console.log("Params: ", params);
-
-        // Ejecuta la consulta
-        const result = await connection.query(query, params);
-        res.status(201).json({ message: "Habitación creada exitosamente", id: result.insertId });
-    } catch (error) {
-        console.error("Error en la consulta a la base de datos:", error);
-        res.status(500).json({ error: 'Error en la consulta a la base de datos' });
-    } 
-});*/
+        try {
+            console.log("Query: ", query); // Ver la consulta SQL
+            console.log("Params: ", params); // Ver parámetros
+    
+            const result = await connection.query(query, params);
+            res.set('Cache-Control', 'no-store');
+            res.json(result);
+        } catch (error) {
+            console.error(error);
+            res.status(500).json({ error: 'Error en la consulta a la base de datos' });
+        } 
+    
+    })
 
 
 
