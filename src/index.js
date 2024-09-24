b6de2463af2b423d9164d444ef9f6e2a3f0cadc8
iarabravo <iarabravo2017@hotmail.com>
2024-09-24 12:59:07 -0300

    Mensaje describiendo los cambios

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 const express = require ("express");
 const morgan = require ("morgan");
 const database = require ("./database");
+const cors = require ("cors");
 
 //Configuration inicial
 const app = express();
@@ -9,7 +10,9 @@ app.listen(app.get("port"));
 console.log("Escuchando comunicaciones al puerto "+app.get("port"));
 
 //Middlewares
-
+app.use(cors({
+    origin: ["http://127.0.0.1:5501", "http://127.0.0.1:5500"]
+}))
 app.use(morgan("dev"));
 
 //Rutas
@@ -17,6 +20,11 @@ app.get("/habitaciones",async(req,res)=>
 {
     const connection = await database.getConnection();
     const result = await connection.query("SELECT * FROM HABITACION");
-    console.log(result)
-    res.json()
+    res.json(result)
 })
+
+app.get('/', function(request, response) {
+
+    response.send("server funcionando!!");
+
+});
