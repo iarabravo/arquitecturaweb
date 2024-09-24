1f1c96a166b48c58b0819c7a3a897815605a8a04
iarabravo <iarabravo2017@hotmail.com>
2024-09-24 12:37:59 -0300

    Mensaje describiendo los cambios

--- src/database.js
+++ src/database.js
@@ -4,6 +4,7 @@ dotenv.config();
 
 const connection = mysql.createConnection({
     host: process.env.HOST,
+    port : process.env.PORT,
     database: process.env.DATABASE,
     user: process.env.USER,
     password: process.env.PASSWORD
